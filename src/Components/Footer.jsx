0e39c366b2f9c1a25e0e7c1434a36db894ff20e8
Jay1826Mistry <22se02cs028@ppsu.ac.in>
2025-12-30 13:06:48 +0530

    Reapply "initialization"
    This reverts commit d3b858893a170242c25ba3fefe5d2339ed4a180a.

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-black py-6 text-center text-sm text-gray-400">
+      <p>© 2025 WeWatchd. Built for movie lovers.</p>
+    </footer>
+  );
+}
+
+export default Footer;
