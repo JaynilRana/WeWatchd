892b7ce3591a4075fce686485d20eed5796b9f1e
Jay1826Mistry <22se02cs028@ppsu.ac.in>
2025-12-30 12:49:44 +0530

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar";
+import Landing from "./Pages/Landing";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
